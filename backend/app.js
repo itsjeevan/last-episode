1b33aaa3dda7707ddf7a6c0e0bdc2346e9464f6c
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-06-22 10:33:30 -0700

    Implemented token based authentication for login

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express()
 const episodePostsRouter = require('./controllers/episodePosts')
 const episodeCommentsRouter = require('./controllers/episodeComments')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -29,6 +30,8 @@ app.use('/api/users', usersRouter)
 app.use('/api/episodeposts', episodePostsRouter)
 // Episode comments router
 app.use('/api/episodecomments', episodeCommentsRouter)
+// Login router
+app.use('/api/login', loginRouter)
 
 // Unknown endpoint
 app.use(middleware.unknownEndpoint)
---
+++ backend/controllers/login.js
@@ -0,0 +1,48 @@
+// Imports
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+// POST routes
+
+// Login user
+loginRouter.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  // Find user by username
+  const user = await User.findOne({ username })
+
+  // Check if password is correct
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  // If no user found or if incorrect password then return 401
+  if (!user || !passwordCorrect) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  // Create JavaScript object for token
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  // Create token digitally signed using environment variable
+  const token = jwt.sign(
+    userForToken,
+    process.env.JWT_SECRET,
+    { expiresIn: 60 * 60 }
+  )
+
+  // Return token and username
+  response
+    .status(200)
+    .send({ token, username: user.username })
+})
+
+// Exports
+module.exports = loginRouter
