07db5997badec8dc463320d3b96b1aea3375162d
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-06-22 23:11:03 -0700

    Started building frontend with simple axios call to backend

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+const App = () => {
+
+  const [show, setShow] = useState('')
+
+  const handleOnSubmitForm = event => {
+    event.preventDefault()
+    axios
+      .get('http://localhost:3001/api/episodeposts')
+      .then(response => {
+        console.log(response.data)
+      })
+  }
+
+  const handleOnChangeShow = event => {
+    setShow(event.target.value)
+  }
+
+  return (
+    <form onSubmit={handleOnSubmitForm}>
+      <div>show: <input value={show} onChange={handleOnChangeShow} /></div>
+      <button type="submit">submit</button>
+    </form>
+  )
+}
+
+export default App
