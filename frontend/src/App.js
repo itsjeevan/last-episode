2606ce3a3a31c2fc5d4eb2738719915ea89f6184
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-06-24 23:57:21 -0700

    Added season and episode images to search results

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,23 +14,25 @@ const App = () => {
     const searchResult = await axios.get(`https://api.themoviedb.org/3/search/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&query=${show}&include_adult=false`)
     setShowList(searchResult.data.results)
     setSeasonList([])
-    setEpisodeList([]) 
+    setEpisodeList([])
+    console.log(searchResult.data)
   }
     
   const handleOnChangeShow = event => {
     setShow(event.target.value)
   }
 
-  const handleOnClickShow = async id => {
-    const seasons = await axios.get(`https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+  const handleOnClickShow = async showId => {
+    const seasons = await axios.get(`https://api.themoviedb.org/3/tv/${showId}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
     setEpisodeList([])
     setSeasonList(seasons.data.seasons)
-    setShowId(id)
-    
+    setShowId(showId)
+    console.log(seasons.data)
   }
-
+  
   const handleOnClickSeason = async (seasonNumber) => {
     const seasonDetail = await axios.get(`https://api.themoviedb.org/3/tv/${showId}/season/${seasonNumber}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    console.log(seasonDetail.data)
     setEpisodeList(seasonDetail.data.episodes)
   }
 
@@ -47,7 +49,7 @@ const App = () => {
           return (
             <li key={show.id} onClick={() => handleOnClickShow(show.id)}>
               {show.name} ({show.first_air_date.substring(0, 4)})
-              <img alt="" src={`https://image.tmdb.org/t/p/w500/${show.backdrop_path}`} />
+              <img width="75" alt="" src={`https://image.tmdb.org/t/p/w500/${show.backdrop_path}`} />
             </li>
             )
           })}
@@ -58,6 +60,7 @@ const App = () => {
           return (
             <li key={season.id} onClick={() => handleOnClickSeason(season.season_number)}>
               {season.name}
+              <img width="75" alt="" src={`https://image.tmdb.org/t/p/w500/${season.poster_path}`} />
             </li>
             )
           })}
@@ -67,7 +70,11 @@ const App = () => {
         {episodeList.map(episode => {
           return (
             <li key={episode.id}>
-              {episode.name}
+              <div>
+                {episode.episode_number}. {episode.name}
+              </div>
+              <img width="75" alt="" src={`https://image.tmdb.org/t/p/w500/${episode.still_path}`} />
+              <p>{episode.overview}</p>
             </li>
             )
           })}
