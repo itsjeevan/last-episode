dbf405568da519ee6d697d187e38651b0dd3fc2e
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-06-26 16:45:56 -0700

    More refactoring

--- frontend/src/components/Season.js
+++ frontend/src/components/Season.js
@@ -1,6 +1,24 @@
-const Season = ({ season, onClickSeason }) => {
+import axios from 'axios'
+
+const Season = ({
+  season,
+  showId,
+  setEpisodes,
+  setSeasonId,
+  setSeasonNumber,
+  setSeasonImage
+}) => {
+
+  const handleOnClickSeason = async (season) => {
+    const seasonDetail = await axios.get(`https://api.themoviedb.org/3/tv/${showId}/season/${season.season_number}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
+    setEpisodes(seasonDetail.data.episodes)
+    setSeasonId(season.id)
+    setSeasonNumber(season.season_number)
+    setSeasonImage(season.poster_path)
+  }
+
   return (
-    <div onClick={onClickSeason}>
+    <div onClick={() => handleOnClickSeason(season)}>
       Season {season.season_number}
       <img width="75" alt="" src={`https://image.tmdb.org/t/p/w500/${season.poster_path}`} />
     </div>
--- frontend/src/components/Seasons.js
+++ frontend/src/components/Seasons.js
@@ -1,5 +1,4 @@
 import Season from './Season'
-import axios from 'axios'
 
 const Seasons = ({
   seasons,
@@ -10,14 +9,6 @@ const Seasons = ({
   setSeasonImage
 }) => {
 
-  const handleOnClickSeason = async (season) => {
-    const seasonDetail = await axios.get(`https://api.themoviedb.org/3/tv/${showId}/season/${season.season_number}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`)
-    setEpisodes(seasonDetail.data.episodes)
-    setSeasonId(season.id)
-    setSeasonNumber(season.season_number)
-    setSeasonImage(season.poster_path)
-  }
-
   return (
     <div>
       <h1>Seasons</h1>
@@ -26,7 +17,11 @@ const Seasons = ({
           <Season
             key={season.id}
             season={season}
-            onClickSeason={() => handleOnClickSeason(season)}
+            showId={showId}
+            setEpisodes={setEpisodes}
+            setSeasonId={setSeasonId}
+            setSeasonNumber={setSeasonNumber}
+            setSeasonImage={setSeasonImage}
           />
         )
       })}
