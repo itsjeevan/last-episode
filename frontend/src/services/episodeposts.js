fdafe5487fe4b6073db102e84bdbd17fe772f49b
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-06-27 23:25:22 -0700

    Added getAll function to retrieve all episode posts

--- frontend/src/services/episodeposts.js
+++ frontend/src/services/episodeposts.js
@@ -2,10 +2,15 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/api/episodeposts'
 
+const getAll = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
 const create = async episodePostData => {
   const response = await axios.post(baseUrl, episodePostData)
   return response.data
 }
 
-const exports = { create }
+const exports = { getAll, create }
 export default exports
