1790413d512881045052a2d8b3209fe864b7e3bc
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-07-14 21:26:30 -0700

    Added css normalize and moved global styles to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import { Normalize } from 'styled-normalize'
+import { ThemeProvider } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
+
+// Styles
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: white;
+    font-family: 'Roboto Condensed', sans-serif; 
+  }
+`
+
+const theme = {
+  spaceLarge: '40px',
+  spaceMedium: '20px',
+  spaceSmall: '10px'
+}
 
 // Render
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
-    <App />
+    <Normalize />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>
 )
