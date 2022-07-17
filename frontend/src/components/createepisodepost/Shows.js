2672cdd4bf7fb1c3fe0c209026dbb171561c9f62
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-07-17 00:38:18 -0700

    Styled shows

--- frontend/src/components/createepisodepost/Shows.js
+++ frontend/src/components/createepisodepost/Shows.js
@@ -1,21 +1,36 @@
 // Import
 import Show from './Show'
+import styled from 'styled-components'
 
 // List of shows
 const Shows = ({ shows, setShowSelected, setSeasons, setEpisodes }) => (
-  <div>
-    <h1>Shows</h1>
-    {shows.map(show => (
-      <Show
-        key={show.id}
-        show={show}
-        setShowSelected={setShowSelected}
-        setSeasons={setSeasons}
-        setEpisodes={setEpisodes}
-      />
-    ))}
-  </div>
+  <>
+    <Heading>Shows</Heading>
+    <Container>
+      {shows.map(show => (
+        <Show
+          key={show.id}
+          show={show}
+          setShowSelected={setShowSelected}
+          setSeasons={setSeasons}
+          setEpisodes={setEpisodes}
+        />
+      ))}
+    </Container>
+  </>
 )
+
+// Styles
+const Heading = styled.h1`
+  margin-top: ${props => props.theme.space.large};
+`
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: start;
+  
+`
 
 // Export
 export default Shows
