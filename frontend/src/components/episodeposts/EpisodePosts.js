c2d644dafb1e6e36ed4ab7cee6a62f57406ba3d0
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-08-05 10:49:18 -0700

    Factored out single episode posts into a new component

--- frontend/src/components/episodeposts/EpisodePosts.js
+++ frontend/src/components/episodeposts/EpisodePosts.js
@@ -1,8 +1,7 @@
 // Imports
-import { Link } from 'react-router-dom'
 import styled from 'styled-components'
-import notfound from '../../assets/404.jpg'
 import { useState } from 'react'
+import EpisodePostSingle from './EpisodePostSingle'
 
 // List of episode posts
 const EpisodePosts = ({ episodePosts, filteredEpisodePosts, setFilteredEpisodePosts }) => {
@@ -40,24 +39,7 @@ const EpisodePosts = ({ episodePosts, filteredEpisodePosts, setFilteredEpisodePo
         {
           filteredEpisodePosts.length !== 0
             ? filteredEpisodePosts.map(episodePost => (
-              <SubContainer to={`/episodepost/${episodePost.id}`} key={episodePost.id}>
-                <Image
-                  alt={episodePost.showName}
-                  src={`https://image.tmdb.org/t/p/w500/${episodePost.episodeImage}`}
-                  onError={({ currentTarget }) => {
-                    currentTarget.onerror = null
-                    currentTarget.src = notfound
-                  }}
-                />
-                <TextContainer>
-                  <ShowName>{episodePost.showName}</ShowName>
-                  <ShowSeason>Season {episodePost.seasonNumber} Episode {episodePost.episodeNumber}: {episodePost.episodeName}</ShowSeason>
-                  <ShowInfo>{episodePost.episodeInfo
-                    ? episodePost.episodeInfo
-                    : 'No episode info found.'
-                  }</ShowInfo>
-                </TextContainer>
-              </SubContainer>
+              <EpisodePostSingle key={episodePost.id} episodePost={episodePost} />
             )).reverse()
             : <Text>No results</Text>
         }
@@ -72,37 +54,6 @@ const Container = styled.div`
   flex-direction: row;
   flex-wrap: wrap;
   gap: 40px;
-`
-const SubContainer = styled(Link)`
-  display: flex;
-  flex-direction: column;
-  width: calc(50% - 20px);
-  height: fit-content;
-  border-radius: ${props => props.theme.radius};
-  cursor: pointer;
-  color: white;
-  text-decoration: none;
-  &:hover {
-    ${props => props.theme.highlight}
-  }
-`
-const Image = styled.img`
-  border-radius: ${props => props.theme.radius} ${props => props.theme.radius} 0 0;
-  width: 100%;
-`
-const TextContainer = styled.div`
-  padding: ${props => props.theme.space.large};
-  background: ${props => props.theme.color.secondary};
-  border-radius: 0 0 ${props => props.theme.radius} ${props => props.theme.radius};
-`
-const ShowName = styled.p`
-  font-size: 40px;
-`
-const ShowSeason = styled.p`
-  margin: ${props => props.theme.space.medium} 0;
-`
-const ShowInfo = styled.p`
-  font-weight: 300;
 `
 const Filter = styled.div`
   display: flex;
--- frontend/src/components/user/User.js
+++ frontend/src/components/user/User.js
@@ -1,5 +1,8 @@
+// Imports
 import { useEffect, useState } from 'react'
 import userService from '../../services/users'
+import styled from 'styled-components'
+import EpisodePostSingle from '../episodeposts/EpisodePostSingle'
 
 const User = () => {
 
@@ -15,27 +18,28 @@ const User = () => {
 
   return (
     <>
-      <h1>User data</h1>
-      {
-        episodePostsCommented.map(episodePost => (
-          <div to={`/episodepost/${episodePost.id}`} key={episodePost.id}>
-            <img
-              alt={episodePost.showName}
-              src={`https://image.tmdb.org/t/p/w500/${episodePost.episodeImage}`}
-            />
-            <div>
-              <p>{episodePost.showName}</p>
-              <p>Season {episodePost.seasonNumber} Episode {episodePost.episodeNumber}: {episodePost.episodeName}</p>
-              <p>{episodePost.episodeInfo
-                ? episodePost.episodeInfo
-                : 'No episode info found.'
-              }</p>
-            </div>
-          </div>
-        )).reverse()
-      }
+      <Heading>You commented on</Heading>
+      <Container>
+        {
+          episodePostsCommented.map(episodePost => (
+            <EpisodePostSingle key={episodePost.id} episodePost={episodePost} />
+          )).reverse()
+        }
+      </Container>
     </>
   )
 }
 
+// Styles
+const Heading = styled.h1`
+  margin-bottom: ${props => props.theme.space.large};
+`
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 40px;
+`
+
+// Export
 export default User
