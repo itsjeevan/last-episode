5a105c22acdabe33cf9e1ce11d04c710d204feb7
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-08-09 16:35:27 -0700

    Added an error handler to middleware for duplicate entries in database

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -6,13 +6,17 @@ const unknownEndpoint = (request, response) => {
 // Error handler
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
+  // If unique entry exists in MongoDB already
+  if (error.name === 'MongoServerError') {
+    return response.status(400).send({ error: 'duplicate entry in db' })
+  }
   // If accessing an incorrect 'id'
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
   // If data entered into MongoDB doesn't pass validation
   else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: 'data not valid for mongodb' })
   }
   // If JWT error
   else if (error.name === 'JsonWebTokenError') {
