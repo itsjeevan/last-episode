d9a063c964e5b333e4d2ddfe340b7b2584ac57d3
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-08-12 16:22:43 -0700

    Header component is responsive

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -18,23 +18,27 @@ const Header = ({ user, setUser }) => {
 
   return (
     <Container>
-      <SubContainer>
+      <LogoContainer>
         <Logo src={logo} alt="Logo" />
         <Title>Last Episode</Title>
-        {/* Create links that modify url */}
-        <Link to="/">Browse</Link>
-        <Link to="/create">Create</Link>
-      </SubContainer>
-      <SubContainer>
-        {user
-          ?
-          <>
-            <Link to="/user">{user.username}</Link>
-            <Logout href="" onClick={handleOnClickLogout}>Logout</Logout>
-          </>
-          : <Link to="/login">Login</Link>
-        }
-      </SubContainer>
+      </LogoContainer>
+      <ContentContainer>
+        <SubContainer>
+          {/* Create links that modify url */}
+          <Link to="/">Browse</Link>
+          <Link to="/create">Create</Link>
+        </SubContainer>
+        <SubContainer>
+          {user
+            ?
+            <>
+              <Link to="/user">{user.username}</Link>
+              <Logout href="" onClick={handleOnClickLogout}>Logout</Logout>
+            </>
+            : <Link to="/login">Login</Link>
+          }
+        </SubContainer>
+      </ContentContainer>
     </Container>
   )
 }
@@ -44,24 +48,49 @@ const Container = styled.nav`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
-  height: 125px;
-  border-bottom: 1px solid ${props => props.theme.color.secondary};
+  padding: ${props => props.theme.space.large} 0;
+  border-bottom: 2px solid ${props => props.theme.color.secondary};
   margin-bottom: ${props => props.theme.space.large};
+  gap: 40px;
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  @media screen and (max-width: 575px) {
+    align-items: center;
+  }
+`
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  justify-content: space-between;
+  @media screen and (max-width: 575px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
+`
+const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: ${props => props.theme.space.medium}; 
 `
 const SubContainer = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  gap: ${props => props.theme.space.small};
 `
 const Logo = styled.img`
   width: 75px;
-  margin-right: ${props => props.theme.space.medium};
 `
 const Title = styled.p`
   font-weight: bold;
   text-transform: uppercase;
-  margin-right: calc(${props => props.theme.space.large} - ${props => props.theme.space.small})
+  white-space: nowrap;
 `
 const Link = styled(NavLink)`
   font-size: 20px;
@@ -69,7 +98,6 @@ const Link = styled(NavLink)`
   color: white;
   padding: ${props => props.theme.space.small} ${props => props.theme.space.medium};
   border-radius: ${props => props.theme.radius};
-  margin-left: ${props => props.theme.space.small};
   &:hover,
   &[class*="active"] {
     background-color: ${props => props.theme.color.tertiary};
@@ -78,7 +106,6 @@ const Link = styled(NavLink)`
 const Logout = styled.button`
   height: auto;
   padding: ${props => props.theme.space.small} ${props => props.theme.space.medium};
-  margin-left: ${props => props.theme.space.small};
 `
 
 // Export
