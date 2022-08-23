4e808df54c48610f18f5e1604c8a756523f19d60
itsjeevan <42787362+itsjeevan@users.noreply.github.com>
2022-08-22 17:54:55 -0700

    Updated way port is assigned

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const http = require('http')
 
 // Server
 const server = http.createServer(app)
-const PORT = 3001
-server.listen(3001, () => {
+const PORT = process.env.PORT || 3001
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
